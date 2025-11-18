3911c86baf4cc8b2080b9129e917d6162b299ccd
EvansRobbie <evansrobbie5311@gmail.com>
2025-11-18 08:22:25 +0300

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Playfair_Display } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components/navs/navbar';
 import { Newsletter } from '@/components/navs/newsletter';
+import { DATA } from '@/_data/data';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,9 +30,48 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-  title: 'Palm Oasis Resort - Best Hotel in Garissa Kenya',
-  description:
-    'Experience luxury and comfort at Palm Oasis Resort, the best hotel in Garissa, Kenya. Enjoy top-notch amenities, exquisite rooms, and exceptional service for an unforgettable stay.',
+  title: `${DATA.title} - Best Hotel in Garissa Kenya`,
+  description: DATA.description,
+  keywords: [
+    'Palm Oasis Resort',
+    'Best Hotel in Garissa',
+    'Luxury Accommodation Garissa',
+    'Garissa Hotels',
+    'Resorts in Garissa Kenya',
+    'Hotel with Conference Facilities Garissa',
+    'Wedding Venues Garissa',
+    'Health Club Garissa',
+    'Fine Dining Garissa',
+    'Affordable Luxury Hotels Kenya',
+  ],
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
+  openGraph: {
+    title: DATA.title,
+    description: DATA.description,
+    images: [
+      {
+        url: DATA.logo,
+        width: 800,
+        height: 600,
+        alt: DATA.title,
+      },
+    ],
+    siteName: DATA.title,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: DATA.title,
+    images: [DATA.logo],
+    description: DATA.description,
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
