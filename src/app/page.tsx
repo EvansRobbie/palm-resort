2fd8b64bd9b0e7ed27b11b1e73e97ad40180e0bb
EvansRobbie <evansrobbie5311@gmail.com>
2025-11-19 23:20:40 +0300

    add gallery section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { ConferencesEvents } from '@/components/home/conference-events';
+import { Gallery } from '@/components/home/gallery';
 import { HealthClub } from '@/components/home/health-club';
 import { Hero } from '@/components/home/hero';
 import { Introduction } from '@/components/home/introduction';
 import PartnersSection from '@/components/home/partners';
 import { Rooms } from '@/components/home/rooms';
-import { Wedding } from '@/components/home/wedding-section';
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
       <Introduction />
       <ConferencesEvents />
       <Rooms />
-      <Wedding />
+      <Gallery />
       <HealthClub />
       <PartnersSection />
     </div>
